9e8f3bb5b09ccfcde6cc5adbdbc5db73b5c1d0a1
Filip <jabos123@gmail.com>
2018-10-22 18:50:03 +0200

    Allowed checks where border  is not set

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import Set, Tuple
-from tiles import tile_costs, amount_of_impassable
+from typing import Set, Tuple, List
+from tiles import tile_costs, amount_of_impassable, is_impassable
 import networkx as nx
 from os import sys
 from directions import get_dir, get_boost_value, get_elevation_stream_direction, opposite_directions
@@ -50,14 +50,14 @@ def place_tile_types_in_board_state(state):
 def map_to_board_optimization(state):
     pass
 
+
 def add_neighbours(tiles, G, own: Tuple[int, int]):
     neighbours = np.array([None, None, None, None], None)
     own_tile = tiles[own[1]][own[0]]["type"]
 
-    am_i_out_of_bounds = own[1]<0 or own[1]>=100 or own[0]<0 or own[0]>=100
-    if am_i_out_of_bounds:
-        print(own[1],own[0])
-    if(own_tile == "forest" or am_i_out_of_bounds or len(G.edges(own)) == 4 ):
+    am_i_out_of_bounds = not (
+        own[1] >= 0 and own[1] < 100 and own[0] >= 0 and own[0] < 100)
+    if(is_impassable(own_tile) or am_i_out_of_bounds or len(G.edges(own)) == 4):
         return None
     else:
         neighbours[0] = (own[0]+1, own[1])
@@ -65,8 +65,11 @@ def add_neighbours(tiles, G, own: Tuple[int, int]):
         neighbours[2] = (own[0]-1, own[1])
         neighbours[3] = (own[0], own[1]-1)
         for i in range(len(neighbours)):
-            if neighbours[i] is not None:
+            x, y = neighbours[i]
+            if (x >= 0 and x < 100 and y >= 0 and y < 100):
                 create_connection(G, own, neighbours[i], tiles)
+            else:
+                neighbours[i] = None
 
     return neighbours
 
@@ -94,11 +97,15 @@ def generate_graph(state, currentPos: Tuple[int, int]):
     G = nx.DiGraph()
     G.add_node(currentPos)
     added_nodes = [currentPos]
+    checked_nodes = set()
     while added_nodes:
-        neighbours = add_neighbours(state["tileInfo"], G, added_nodes.pop(-1))
+        next_to_add = added_nodes.pop(-1)
+        checked_nodes.add(next_to_add)
+        neighbours = add_neighbours(
+            state["tileInfo"], G, next_to_add)
         if neighbours is not None:
             for val in neighbours:
-                if val != None:
+                if val != None and val not in checked_nodes:
                     added_nodes.append(val)
     return G
 
